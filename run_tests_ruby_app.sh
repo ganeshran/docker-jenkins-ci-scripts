06be9d2f39228c949c063384998edc8bffe0dc3b
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-04-09 23:45:48 +0530

    backing up data manually

--- run_tests_ruby_app.sh
+++ run_tests_ruby_app.sh
@@ -4,6 +4,10 @@
 
 echo -e "Stopped existing docker compose instances"
 
+tar -cvzf dbdata.tar /dbdata
+
+echo -e  "Took backup of existing data"
+
 /usr/local/bin/docker-compose build
 
 echo -e "Built Docker Image"
@@ -11,6 +15,12 @@ echo -e "Built Docker Image"
 /usr/local/bin/docker-compose up -d
 
 echo -e "Brought up docker compose "
+
+rm /dbdata/*
+
+echo -e "removed all files in mounted volume"
+
+tar -cf dbdata.tar /dbdata
 
 /usr/local/bin/docker-compose run web rake db:migrate
 

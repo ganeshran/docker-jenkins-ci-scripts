37158d44ca1d77b2f5bc9dedbc76d6418a3ba53a
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-04-09 20:55:44 +0530

    Update run_tests_ruby_app.sh

--- run_tests_ruby_app.sh
+++ run_tests_ruby_app.sh
@@ -2,6 +2,16 @@
 
 /usr/local/bin/docker-compose stop
 
+echo -e "Stopped existing docker compose instances"
+
 /usr/local/bin/docker-compose build
 
-/usr/local/bin/docker-compose up
+echo -e "Built Docker Image"
+
+/usr/local/bin/docker-compose up -d
+
+echo -e "Brought up docker compose "
+
+/usr/local/bin/docker-compose run web rake db:migrate
+
+echo -e "DB Migration is complete"

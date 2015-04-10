b11db96603bb2fa6efe16b6b2eac1782644dbf99
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2015-04-10 11:31:48 +0530

    Update run_tests_ruby_app.sh

--- run_tests_ruby_app.sh
+++ run_tests_ruby_app.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-pg_dump -c -h localhost -p 49153 -U postgres > backup.sql
-
-echo -e "Take backup of the entire database"
-
 /usr/local/bin/docker-compose stop
 
 echo -e "Stopped existing docker compose instances"
-
-echo -e  "Took backup of existing data"
 
 /usr/local/bin/docker-compose build
 
 echo -e "Built Docker Image"
 
-/usr/local/bin/docker-compose up -d
+/usr/local/bin/docker-compose run web -d
 
 echo -e "Brought up docker compose "
-
-psql -h localhost -p 49153 -U postgres < backup.sql
-
-echo -e "Restoring entire db now"
 
 /usr/local/bin/docker-compose run web rake db:migrate
 
